6526b5edbf84989f2e79094e0d808fbf5de18230
Deusyvan Silva <contato@dfsweb.com.br>
2020-01-13 23:06:44 -0300

    Atualização

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@home');
+
+Route::get('/algo', 'HomeController@teste');
+
+Route::get('/algo/{id}', 'HomeController@teste2');
